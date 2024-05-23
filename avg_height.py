05920827fb686d10769ecfb60aabc3d8673f9cf6
Dhanushree Motanalira Somaiah <motanalirasomaiahd@cardiff.ac.uk>
2024-05-23 21:00:16 +0100

    final avg height

--- avg_height.py
+++ avg_height.py
@@ -29,4 +29,16 @@ student_heights = input().split()
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
 
-  
+total_height = 0
+for height in student_heights:
+  total_height += height
+print(f"total height = {total_height}")    
+
+# total_students = len(student_heights)
+total_students = 0
+for student in student_heights:
+  total_students += 1
+print(f"total students = {total_students}") 
+
+avg_height = total_height / len(student_heights)
+print(f"average height = {avg_height}") 
